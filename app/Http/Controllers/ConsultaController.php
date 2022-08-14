c453f5b20147c4ee4a423927f1d9f57db3cde2fc
Anderson Belderrama <andersonbelderrama@gmail.com>
2022-08-13 21:23:05 -0300

    separando e comparando nomes

--- app/Http/Controllers/ConsultaController.php
+++ app/Http/Controllers/ConsultaController.php
@@ -11,14 +11,36 @@ class ConsultaController extends Controller
     //consulta_nome
     public function consulta_nome(Request $request)
     {
+        //pega o nome da pessoa que esta sendo consultada
         $nome = $request->input('nome');
-        $nome_lista = ItemLista::with('lista:id,nome as lista')->where('nome', 'like', '%' . $nome . '%')->get(['nome','documento','motivo','lista_id']);
 
+        //transforma nome em array de nomes
+        $nome_explode = explode(' ', $nome);
+
+        //buscando nome na lista
+        $nome_lista = ItemLista::with('lista:id,nome as lista')
+                    ->where('nome', 'like', '%' . $nome . '%')
+                    ->orWhere([
+                        ['nome', 'like', '%' . $nome_explode[0] . '%'],
+                        ['nome', 'like', '%' . $nome_explode[1] . '%']
+                    ])
+                    ->orWhere([
+                        ['nome', 'like', '%' . $nome_explode[2] . '%'],
+                        ['nome', 'like', '%' . $nome_explode[3] . '%']
+                    ])
+                    ->orWhere([
+                        ['nome', 'like', '%' . $nome_explode[4] . '%'],
+                        ['nome', 'like', '%' . $nome_explode[5] . '%']
+                    ])
+                    ->get(['nome','documento','motivo','lista_id']);
+
+        //monta resultado de retorno da consulta
         $resultado = [
             'nome' => $nome,
             'similar_text' => similar_text("Osama bin Mohammed bin Awad bin Laden", "Usamah Bin Muhammad bin Awad bin Ladin", $percent),
             'percent' => $percent,
-            'nome_lista' => $nome_lista
+            'nome_lista' => $nome_lista,
+            'nome_explode' => $nome_explode
         ];
 
 
